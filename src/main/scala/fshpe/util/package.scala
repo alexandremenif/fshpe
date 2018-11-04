8d20c85503ee00fa4530679b5105453d775bba3b
Alexandre Menif <alexandre@menif.name>
2018-11-04 14:37:49 +0100

    feat: add utility function to measure elapsed time

---
+++ src/main/scala/fshpe/util/package.scala
@@ -0,0 +1,13 @@
+package fshpe
+
+package object util {
+
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + ((t1 - t0) / 1000).toDouble / 1000 + "ms")
+    result
+  }
+
+}
